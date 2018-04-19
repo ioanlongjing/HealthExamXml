1f8641a0c649a9560d2f1dd77fbd00603ca6713d
ioanlongjing <ioan.snickersnee@gmail.com>
2018-04-19 14:00:37 +0800

    Check the file context is null ?

--- HealthExamXml.py
+++ HealthExamXml.py
@@ -5,9 +5,15 @@ from lxml import etree
 
 for name in glob.glob('un/*.xml'):
 	print( name )
-	page = etree.parse( name )
-	for nodeOffice in page.xpath('//Check[@Name="Office Security Updates"]/Advice'):
-		print(" The Office Security Updata is %s "  % nodeOffice.text )
+        try:
+            page = etree.parse( name )
+            for nodeOffice in page.xpath('//Check[@Name="Office Security Updates"]/Advice'):
+                    print(" The Office Security Updata is %s "  % nodeOffice.text )
 
-	for nodeWindows in page.xpath('//Check[@Name="Windows Security Updates"]/Advice'):
-		print(" The Windows Seucrity Update is %s " %nodeOffice.text )
+            for nodeWindows in page.xpath('//Check[@Name="Windows Security Updates"]/Advice'):
+                    print(" The Windows Seucrity Update is %s " %nodeOffice.text )
+
+        except:
+            print( " The File context is null." )
+            print( name )
+            continue
