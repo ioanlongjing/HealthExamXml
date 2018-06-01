2aefd00d216f82169841f86ea68791d65efa8ba6
ioanlongjing <ioan.snickersnee@gmail.com>
2018-06-01 15:21:21 +0800

    修改 HealthExamXml.py and README.txt

--- HealthExamXml.py
+++ HealthExamXml.py
@@ -4,16 +4,29 @@ import glob
 from lxml import etree
 
 for name in glob.glob('un/*.xml'):
-	print( name )
+        print(" \n ")
+        print( name )
         try:
-            page = etree.parse( name )
-            for nodeOffice in page.xpath('//Check[@Name="Office Security Updates"]/Advice'):
-                    print(" The Office Security Updata is %s "  % nodeOffice.text )
+                page = etree.parse( name )
+                Windows_Sec_update = '//Check[(@Name="Windows Security Updates" and @ID="500")]'
 
-            for nodeWindows in page.xpath('//Check[@Name="Windows Security Updates"]/Advice'):
-                    print(" The Windows Seucrity Update is %s " %nodeWindows.text )
-
-        except:
-            print( " The File context is null." )
-            print( name )
-            continue
+                Windows_Sec_Update = page.xpath( Windows_Sec_update )
+                for Window_Check in Windows_Sec_Update:
+                        print '========'
+                        advices = Window_Check.findall("./Advice")
+                        for advice in advices:
+                                print '[ Advice ] : '+advice.text
+                        updatedDatas = Window_Check.findall("./Detail/UpdateData[@IsInstalled=\"false\"]")
+                        for update in updatedDatas:
+                                titles = update.findall("./Title")
+                                for title in titles:
+                                        print '[ title ] : '+ title.text
+                                details = update.findall("./References/InformationURL")
+                                for detail in details:
+                                        print '[ Detail ] : '+ detail.text
+                        print '========'
+        except Exception as e:
+                print e
+                print( " The File context is null." )
+                print( name )
+                continue
--- README.txt
+++ README.txt
@@ -2,5 +2,5 @@
 因為有時遇到健檢得資料會很多 , 懶得一個個慢慢來
 這邊需要安裝 lxml ( Ubuntu 為例)
 	( python 2.7 )
-	sudo apt-get instyall python-lxml
+	sudo apt-get install python-lxml
 之後 create 一個 un 的 folder , 將 .xml 都扔進去執行就好惹
